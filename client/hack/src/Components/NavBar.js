d4f93afe8c0d2358bc26f10c4adbb9aacf6aa46d
ArkoniX21 <chernovsergey21@yandex.ru>
2022-03-18 19:35:10 +0300

    3

---
+++ client/hack/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+const NavBar = () => {
+    return (
+        <Navbar>
+            <Nav.Link></Nav.Link>
+        </Navbar>
+    );
+};
+
+export default NavBar;
